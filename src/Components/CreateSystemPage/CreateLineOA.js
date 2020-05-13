03d03817c0d6780190f314ae0614128c7d26669e
panupongjkn <panupong.jkn@gmail.com>
2020-05-14 05:48:54 +0700

    [CRAETE] create system and connect line oa

---
+++ src/Components/CreateSystemPage/CreateLineOA.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { Input, Checkbox } from 'antd'
+class CreateLineOA extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div {...this.props}>
+                <Checkbox
+                    onChange={this.props.onCheckLineOA}
+                    checked={this.props.system.checkLineOA}
+                >
+                    LineOA
+                </Checkbox>
+                <div className="ml-5" >
+                    {this.props.system.lineOA.map((lineoa, key) => {
+                        return (
+                            <div className="pt-2">
+                                <p>Line OA ({key + 1})</p>
+                                <Input
+                                    disabled={this.props.system.checkLineOA ? false : true}
+                                    onChange={(e) => this.props.onChangeLineOA(e, key)}
+                                    name="lineoaname"
+                                    value={lineoa.lineoaname}
+                                    required
+                                    placeholder="Channel Name" type="text" />
+                                <Input
+                                    disabled={this.props.system.checkLineOA ? false : true}
+                                    onChange={(e) => this.props.onChangeLineOA(e, key)}
+                                    name="channelid"
+                                    value={lineoa.channelid}
+                                    className="mt-1"
+                                    required
+                                    placeholder="Channel ID"
+                                    type="text" />
+                                <Input
+                                    disabled={this.props.system.checkLineOA ? false : true}
+                                    onChange={(e) => this.props.onChangeLineOA(e, key)}
+                                    name="channeltoken"
+                                    value={lineoa.channeltoken}
+                                    className="mt-1"
+                                    required
+                                    placeholder="Channel Access Token"
+                                    type="text" />
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        )
+    }
+}
+
+export default CreateLineOA
+
--- src/Pages/CreateSystem.js
+++ src/Pages/CreateSystem.js
@@ -2,45 +2,99 @@ import React from 'react'
 import axios from 'axios'
 import withAuth from "../HOC/withAuth"
 import { Redirect } from 'react-router-dom'
+import { Input } from 'antd'
 
+import CreateLineOA from '../Components/CreateSystemPage/CreateLineOA'
 class CreateSystemPage extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            systemname: ""
+            system: {
+                systemname: "",
+                lineOA: [
+                    {
+                        lineoaname: "",
+                        channelid: "",
+                        channeltoken: "",
+                    }
+                ],
+                checkLineOA: false,
+            },
+            redirect: false
         }
     }
     onChangeName = (e) => {
         let stateName = e.target.name
         let value = e.target.value
-        this.setState({
-            [stateName]: value,
-            redirect: false,
-        })
+        this.setState(prevState => ({
+            system : {
+                ...prevState.system,
+                [stateName]: value,
+            }
+        }))
+    }
+    onChangeLineOA = (e, key) => {
+        let name = e.target.name
+        let value = e.target.value
+        let alllineoa = this.state.system.lineOA
+        let lineoa = alllineoa[key]
+        lineoa = {
+            ...lineoa,
+            [name]: value
+        }
+        alllineoa[key] = lineoa
+        this.setState(prevState => ({
+            system: {
+                ...prevState.system,
+                lineOA: alllineoa
+            }
+        }))
     }
     onCreateSystem = (event) => {
-        let data = new FormData()
-        data.append("systemname", this.state.systemname)
+        let data = {
+            systemname: this.state.system.systemname,
+            lineOA: this.state.system.lineOA
+        }
+        console.log(data)
         axios.post('http://localhost:8080/system/create', data, {
-            headers : {
-                'Authorization': "Bearer "+localStorage.getItem("JWT")
+            headers: {
+                'Authorization': "Bearer " + localStorage.getItem("JWT")
             }
         }).then(res => {
-            this.setState({redirect: true})
+            this.setState({ redirect: true })
         })
         event.preventDefault();
     }
+    onCheckLineOA = () => {
+        this.setState(prevState => ({
+            system : {
+                ...prevState.system,
+                checkLineOA: !this.state.system.checkLineOA
+            }
+        }))
+    }
     render() {
         if (this.state.redirect) {
-            return <Redirect push to="/allsystem"/>
+            return <Redirect push to="/allsystem" />
         }
         return (
-            <div>
-                <form onSubmit={this.onCreateSystem}>
-                    <label>System name :</label>
-                    <input name="systemname" value={this.state.systemname} onChange={this.onChangeName} type="text"/>
-                    <button className="btn btn-success" type="submit">Create</button>
-                </form>
+            <div className="container  pt-3">
+                <div className="col-8 mx-auto">
+                    <h3>Create System</h3>
+                    <form onSubmit={this.onCreateSystem}>
+                        <Input
+                            required
+                            name="systemname"
+                            value={this.state.system.systemname}
+                            placeholder="System Name"
+                            onChange={this.onChangeName} type="text" />
+                        <CreateLineOA system={this.state.system} onChangeLineOA={this.onChangeLineOA} onCheckLineOA={this.onCheckLineOA} className="pt-3" />
+                        <div className="d-flex justify-content-between mt-5">
+                            <button className="btn btn-danger">Back</button>
+                            <button className="btn btn-success" type="submit">Create</button>
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
