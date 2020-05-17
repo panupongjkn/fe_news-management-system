435e0bf30a0b7527701ff5d73ad68fcfae6a975b
panupongjkn <panupong.jkn@gmail.com>
2020-05-18 02:04:22 +0700

    [UPDATE]test liff

--- src/Pages/Line/Routes.js
+++ src/Pages/Line/Routes.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import {Redirect} from 'react-router-dom'
+import { Redirect } from 'react-router-dom'
+
+const liff = window.liff
 
 class Routes extends React.Component {
     constructor(props) {
@@ -7,21 +9,65 @@ class Routes extends React.Component {
         this.state = {
             path: "",
             redirect: false,
+            name: '',
+            userLineID: '',
+            pictureUrl: '',
+            statusMessage: '',
+            languageDevice: '',
+            versionSDK: '',
+            client: '',
+            isLogin: '',
+            os: '',
         }
     }
     async componentDidMount() {
-        const queryString = decodeURIComponent(window.location.search).replace("?liff.state=", "");
-        let query = new URLSearchParams(queryString)
-        let path = "/line/"+query.get("system")+"/"+query.get("systemid")+"/register"
-        await this.setState({
-            path: path,
-            redirect: true
-        })
+        // const queryString = decodeURIComponent(window.location.search).replace("?liff.state=", "");
+        // let query = new URLSearchParams(queryString)
+        // let path = "/line/" + query.get("system") + "/" + query.get("systemid") + "/register"
+        // await this.setState({
+        //     path: path,
+        //     redirect: true
+        // })
+        liff.init({ liffId: '1654010598-xR8ZnwJ2' })
+            .then(async () => {
+                this.getProfile()
+                if (!liff.isLoggedIn()) {
+                    liff.login();
+                }
+            })
+            .catch((err) => {
+                console.log(err)
+            });
     }
+    getProfile = async () => {
+        liff.getProfile().then(async dataInfo => {
+            await this.setState({
+                name: dataInfo.displayName,
+                userLineID: dataInfo.userId,
+                pictureUrl: dataInfo.pictureUrl,
+                statusMessage: dataInfo.statusMessage
+            });
+        });
+
+        const languageDevice = liff.getLanguage();
+        const versionSDK = liff.getVersion();
+        const client = liff.isInClient();
+        const isLogin = liff.isLoggedIn();
+        const os = liff.getOS();
+
+        await this.setState({
+            languageDevice: languageDevice,
+            versionSDK: versionSDK,
+            client: (client === true) ? 'YES' : 'NO',
+            isLogin: (isLogin === true) ? 'Login' : 'Not Login',
+            os: os
+        });
+    }
+
     render() {
-        if(this.state.redirect){
-            return <Redirect push to={this.state.path}/>
-        }
+        // if (this.state.redirect) {
+        //     return <Redirect push to={this.state.path} />
+        // }
         return (
             <div>
                 Redirect to {this.state.path}
