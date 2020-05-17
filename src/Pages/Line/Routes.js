a90a50d3b9fff8cefb30e95d1b0ec6f868837f47
panupongjkn <panupong.jkn@gmail.com>
2020-05-17 20:20:49 +0700

    [UPDATE] checl link line

--- src/Pages/Line/Routes.js
+++ src/Pages/Line/Routes.js
@@ -10,7 +10,7 @@ class Routes extends React.Component {
         }
     }
     componentDidMount() {
-        const queryString = decodeURIComponent(window.location.search).replace("?cache=1234?liff.state=", "");
+        const queryString = decodeURIComponent(window.location.search).replace("?liff.state=", "");
         let query = new URLSearchParams(queryString)
         let path = "/line/"+query.get("system")+"/"+query.get("systemid")+"/register"
         this.setState({
@@ -18,7 +18,10 @@ class Routes extends React.Component {
             redirect: true
         })
         console.log("query", window.location.search)
-        console.log("query2", query)
+        console.log("query2", queryString)
+        console.log("query3", query)
+        console.log("system", query.get("system"))
+        console.log("systemid", query.get("systemid"))
     }
     render() {
         // if(this.state.redirect){
