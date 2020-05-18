5a4f9c83f5081d5b27e43727c6b2da9d8cc71196
panupongjkn <panupong.jkn@gmail.com>
2020-05-18 08:46:33 +0700

    liff

--- src/Pages/Line/Routes.js
+++ src/Pages/Line/Routes.js
@@ -18,22 +18,22 @@ class Routes extends React.Component {
     }
     async componentDidMount() {
         // const queryString = await decodeURIComponent(window.location.search).replace("?liff.state=", "");
-        let queryString = window.location.search
-        let query = await new URLSearchParams(queryString.substring(12))
-        let path = await "/line/" + query.get("system") + "/" + query.get("systemid") + "/register"
-        await this.setState({
-            path: path,
-        })
-        await liff.init({ liffId: "1654010598-xR8ZnwJ2" })
-        const profile = await liff.getProfile()
-        await this.setState({
-            line: {
-                displayName: profile.displayName,
-                userId: profile.userId,
-                pictureUrl: profile.pictureUrl,
-                email: liff.getDecodedIDToken().email
-            }
-        })
+        // let queryString = window.location.search
+        // let query = await new URLSearchParams(queryString.substring(12))
+        // let path = await "/line/" + query.get("system") + "/" + query.get("systemid") + "/register"
+        // await this.setState({
+        //     path: path,
+        // })
+        // await liff.init({ liffId: "1654010598-xR8ZnwJ2" })
+        // const profile = await liff.getProfile()
+        // await this.setState({
+        //     line: {
+        //         displayName: profile.displayName,
+        //         userId: profile.userId,
+        //         pictureUrl: profile.pictureUrl,
+        //         email: liff.getDecodedIDToken().email
+        //     }
+        // })
     }
 render() {
     // if (this.state.redirect) {
@@ -41,12 +41,12 @@ render() {
     // }
     return (
         <div>
-            Redirect to {this.state.path}
+            {/* Redirect to {this.state.path}
             <p>userid: {this.state.line.userId}</p>
             <button onClick={this.getProfile}>Get</button>
             <Link to={this.state.path}>
                 <button>Check</button>
-            </Link>
+            </Link> */}
         </div>
     )
 }
