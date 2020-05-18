01cd39c108dfc2cad09020084837d2bd6ae856f1
panupongjkn <panupong.jkn@gmail.com>
2020-05-18 08:36:10 +0700

    liff

--- src/Routes.js
+++ src/Routes.js
@@ -45,7 +45,7 @@ export default function Routes() {
                 <Route path="/:system/:systemid/dashboard" render={(props) => <DashboardPage {...props}/>} />
                 <Route path="/:system/:systemid/role" render={(props) => <RoleUserPage {...props}/>} />
                 <Route path="/test" render={(props) => <Layout {...props}/>} />
-                <Route path="/" render={(props) => <div {...props}>Waiting</div>} />
+                <Route path="/" render={(props) => <LineRoutes {...props}/>} />
                 {/* <Route render={() => <Redirect push to="/login" />} /> */}
             </Switch>
         </Router>
